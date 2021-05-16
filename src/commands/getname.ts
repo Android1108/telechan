3eb5bec3b56943e2e778f887456e2fb177b164b6
Jayden <37990999+Android1108@users.noreply.github.com>
2021-05-16 21:26:35 +0800

    update

---
+++ src/commands/getname.ts
@@ -0,0 +1,14 @@
+import { author, homepage, name, version } from "../../package.json";
+
+const debug = require("debug")("bot:about_command");
+
+const getname= () => (ctx: any) => {
+    
+    const name=  ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
+
+    const message = `*${name}`;
+    debug(`Triggered "about" command with message \n${message}`);
+
+    ctx.reply(message);
+};
+
