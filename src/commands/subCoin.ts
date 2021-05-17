6b61b768e9b4f488cf562a96a9af194ad43e6036
Jayden <37990999+Android1108@users.noreply.github.com>
2021-05-17 18:56:16 +0800

    Update subCoin.ts

--- src/commands/subCoin.ts
+++ src/commands/subCoin.ts
@@ -13,10 +13,11 @@ const subCoin = () => (ctx: any) => {
     replyToMessage(ctx, messageId, `请输入订阅的数字货币名称`);
   }else{
 
-  let url='https://data.block.cc/api/v3/kline?desc=binance_${subCoin}_USDT&type=15m&interval=1m&api_key=YPCNWDHCQYHJTPCTVLEQWUED1IJKWYM7F097TYTU&start=1621228860000'
+  let url='https://data.block.cc/api/v3/kline?desc=binance_'+subCoin+'_USDT&type=15m&interval=1m&api_key=YPCNWDHCQYHJTPCTVLEQWUED1IJKWYM7F097TYTU&start=1621228860000'
 
  axios.get(url)
  .then(function (response) {
+   
    replyToMessage(ctx, messageId, `${coinName}，${response.data[response.data.length-1].T}`);
    console.log(response);
  })
