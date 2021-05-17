eb7a50b427e2f6a8fcf1f22170f515b6ef9c018f
Jayden <37990999+Android1108@users.noreply.github.com>
2021-05-17 23:33:12 +0800

    Update subCoin.ts

--- src/commands/subCoin.ts
+++ src/commands/subCoin.ts
@@ -18,7 +18,36 @@ const subCoin = () => (ctx: any) => {
   if(coinName==""){
     replyToMessage(ctx, messageId, `请输入订阅的数字货币名称`);
   }else{
-  let url='https://data.block.cc/api/v3/kline?desc=huobi-dm_'+coinName+'_USDT&interval=1m&api_key=YPCNWDHCQYHJTPCTVLEQWUED1IJKWYM7F097TYTU&start='+startTime
+  let url='https://data.block.cc/api/v3/kline?desc=okex_'+coinName+'_USDT&interval=1m&api_key=YPCNWDHCQYHJTPCTVLEQWUED1IJKWYM7F097TYTU&start='+startTime
+
+
+
+
+
+  axios.get(url)
+  .then(function (response) {
+    if(response.data instanceof Array){
+      var lastMin=response.data[response.data.length-1];
+      var podong=(lastMin.o-lastMin.c)/lastMin.o;
+      podong=podong*100;
+      podong= Math.round(podong * 100) / 100//四舍五入
+      replyToMessage(ctx, messageId, `${coinName}最新一分钟的涨跌幅是:${podong}%`);
+  
+      console.log(response);
+    }else{
+      replyToMessage(ctx, messageId, `没有找到该币种`);
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+    replyToMessage(ctx, messageId, `没有找到该币种`);
+  });
+
+
+
+
+
+
 
 
   let rule = new schedule.RecurrenceRule();
