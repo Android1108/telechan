9a5a35977862a8e03112fbb609c6f31273d37dd1
Jayden <37990999+Android1108@users.noreply.github.com>
2021-05-17 15:19:26 +0800

    1

--- src/commands/getname.ts
+++ src/commands/getname.ts
@@ -2,7 +2,6 @@
 
 const getname= () => (ctx: any) => {
     const messageData = ctx.message;
-    const messageId = ctx.from.id;
     const name=  ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
 
     const message = `*${name},${messageData}`;
