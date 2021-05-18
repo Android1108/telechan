2793c8bf2ed88a4e5bc8266ec8bf87747f2e5774
Jayden <37990999+Android1108@users.noreply.github.com>
2021-05-18 15:35:00 +0800

    Update subCoin.ts

--- src/commands/subCoin.ts
+++ src/commands/subCoin.ts
@@ -1,6 +1,9 @@
 
 import axios from 'axios';
 const schedule = require('node-schedule');
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const fiveSecond=300000;
+
 
 const replyToMessage = (ctx: any, messageId: string, string: string) =>
   ctx.reply(string, {
@@ -8,74 +11,38 @@ const replyToMessage = (ctx: any, messageId: string, string: string) =>
   });
 
 const subCoin = () => (ctx: any) => {
-  const messageId = ctx.message.message_id;
-  const coinName=ctx.message.text.substr(5);
-  const fiveSecond=300000;
 
-  const startTime=new Date().getTime()-fiveSecond
-  console.log(startTime)
+  const chat_id=ctx.message.chat.id;
+  const coinName=ctx.message.text.substr(5);
+
 
   if(coinName==""){
-    replyToMessage(ctx, messageId, `请输入订阅的数字货币名称`);
+    ctx.telegram.sendMessage(chat_id, `请输入订阅的数字货币名称`)
   }else{
-  let url='https://data.block.cc/api/v3/kline?desc=okex_'+coinName+'_USDT&interval=1m&api_key=YPCNWDHCQYHJTPCTVLEQWUED1IJKWYM7F097TYTU&start='+startTime
+    sendCoinInfo(ctx,chat_id,coinName)
 
 
 
-
-
-  axios.get(url)
-  .then(function (response) {
-    if(response.data instanceof Array){
-      var lastMin=response.data[response.data.length-1];
-      var podong=(lastMin.o-lastMin.c)/lastMin.o;
-      podong=podong*100;
-      podong= Math.round(podong * 100) / 100//四舍五入
-      replyToMessage(ctx, messageId, `${coinName}最新一分钟的涨跌幅是:${podong}%`);
-  
-      console.log(response);
-    }else{
-      replyToMessage(ctx, messageId, `没有找到该币种`);
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-    replyToMessage(ctx, messageId, `没有找到该币种`);
-  });
-
-
-
-
-
-
-
-
-  let rule = new schedule.RecurrenceRule();
-  rule.second = 0;
-  schedule.scheduleJob(rule, () => {
-    axios.get(url)
-    .then(function (response) {
-      if(response.data instanceof Array){
-        var lastMin=response.data[response.data.length-1];
-        var podong=(lastMin.o-lastMin.c)/lastMin.o;
-        podong=podong*100;
-        podong= Math.round(podong * 100) / 100//四舍五入
-        replyToMessage(ctx, messageId, `${coinName}最新一分钟的涨跌幅是:${podong}%`);
+  // let job = schedule.scheduleJob('2 * * * * *', () => {
+  //   axios.get(url)
+  //   .then(function (response) {
+  //     if(response.data instanceof Array){
+  //       var lastMin=response.data[response.data.length-1];
+  //       var podong=(lastMin.o-lastMin.c)/lastMin.o;
+  //       podong=podong*100;
+  //       podong= Math.round(podong * 100) / 100//四舍五入
+  //       replyToMessage(ctx, messageId, `${coinName}最新一分钟的涨跌幅是:${podong}%`);
     
-        console.log(response);
-      }else{
-        replyToMessage(ctx, messageId, `没有找到该币种`);
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-      replyToMessage(ctx, messageId, `没有找到该币种`);
-    });
-  });
-
-
-  
-
+  //       console.log(response);
+  //     }else{
+  //       replyToMessage(ctx, messageId, `没有找到该币种`);
+  //     }
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //     replyToMessage(ctx, messageId, `没有找到该币种`);
+  //   });
+  // });
 
 
 
@@ -88,5 +55,42 @@ const subCoin = () => (ctx: any) => {
   
   // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
 };
+
+
+async function  sendCoinInfo(ctx: any,chat_id:String,coinName:String){
+  // var params = new URLSearchParams();
+  // params.append("chat_id",chat_id);
+  // const ret = await axios.post( "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage" , params );
+  const startTime=new Date().getTime()-fiveSecond
+  let url='https://data.block.cc/api/v3/kline?desc=okex_'+coinName+'_USDT&interval=1m&api_key=YPCNWDHCQYHJTPCTVLEQWUED1IJKWYM7F097TYTU&start='+startTime
+
+  await axios.get(url)
+  .then(function (response) {
+    if(response.data instanceof Array){
+      var state="上涨"
+      var lastMin=response.data[response.data.length-1];
+      var podong=(lastMin.c-lastMin.o)/lastMin.o;
+      podong=podong*100;
+      podong= Math.round(podong * 100) / 100//四舍五入
+      if(podong<0){
+        state="下跌"
+      }else{
+        state="上涨"
+      }
+      ctx.telegram.sendMessage(chat_id, `【${coinName}${state}】最近一分钟的涨跌幅是:${podong}%`)
+
+  
+      console.log(response);
+    }else{
+
+      ctx.telegram.sendMessage(chat_id, `没有找到该币种`)
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+    ctx.telegram.sendMessage(chat_id, `没有找到该币种`)
+  });
+
+}
 
 export {subCoin};
